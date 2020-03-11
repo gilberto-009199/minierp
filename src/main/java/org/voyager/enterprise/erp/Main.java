fdbd985e088d9fd31ddff20b9008c5ffcdcb91f5
Gilberto Ramos de Oliveira <1903991@SALAS.AULAS>
2020-03-10 22:17:48 -0300

    HOME

--- src/main/java/org/voyager/enterprise/erp/Main.java
+++ src/main/java/org/voyager/enterprise/erp/Main.java
@@ -1,13 +1,17 @@
 package org.voyager.enterprise.erp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class ErpApplication {
+//@ComponentScan(basePackages = { "org.voyager.enterprise.erp.controllers" })
+@EnableAutoConfiguration
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ErpApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
